cc1e34415b5aec085ee3c02904b48cc633333047
iccethgamer <iccegames419@gmail.com>
2025-08-14 17:53:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/atby2t.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that your name is ALIVE now—ready to dive into new adventures with you!",
-SUDO_NB: process.env.SUDO_NB || "your number",
+ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that icce-world is ALIVE now—ready to dive into new adventures with you!",
+SUDO_NB: process.env.SUDO_NB || "2348061708739",
 AUTO_REACT: process.env.AUTO_REACT || "true", // Enable/disable auto reaction (true/false)
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"false",
+AUTO_VOICE:"true",
 OMDB_API_KEY: "5e339fb7",
-OWNER_NAME: process.env.OWNER_NAME || "your name",
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+OWNER_NAME: process.env.OWNER_NAME || "icce-world",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true"
 };
 
